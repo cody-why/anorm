06edcf67fb5f04c7611f3c9fc8e8fec642c96d37
Plucky <plucky@example.com>
2023-10-31 18:38:31 +0800

    add sql_args

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 /*
  * @Author: plucky
  * @Date: 2022-10-21 16:53:21
- * @LastEditTime: 2023-10-31 17:51:58
+ * @LastEditTime: 2023-10-31 18:37:10
  * @Description: 
  */
 
@@ -12,7 +12,7 @@
 //! adding the following to your project's Cargo.toml:
 //! ```toml
 //! [dependencies]
-//! co-orm = { virsion = "0.2", features = ["mysql"] }
+//! anorm = { virsion = "0.1", features = ["mysql"] }
 //! sqlx = { version = "0.7", features = ["mysql"] }
 //! ```
 //! features: mysql, postgres, sqlite, mssql
@@ -22,20 +22,18 @@
 //! use anorm::{Crud, FromRow};
 //! 
 //! #[derive(Debug, Crud, FromRow)]
-//! #[orm_rename = "users"] // rename table name
+//! #[anorm(rename = "users")] // rename table name
 //! pub struct User {
-//!     #[orm_seq] // sequence field, insert will ignore this field
-//!     //#[orm_pk] // default first field is primary key
-//!     pub id: u64,
-//! 
-//!     #[orm_by] // generate query_by_field,update_by_field,delete_by_field
-//!     // #[orm_rename = "name"] // rename field name
-//!     pub name: String,
-//!     #[orm_update] // generate method update_xxx. 
-//!     pub password: String,
-//! 
-//!     #[orm_ignore] // ignore field
-//!     pub addr: String,
+//!     // anorm(id) // default first field is primary key
+//!      #[anorm(seq)] // sequence field, insert will ignore this field
+//!      pub id: u64,
+//!      #[anorm(rename = "name")] // rename field name
+//!      #[anorm(by)] // generate query_by_field,update_by_field,delete_by_field
+//!      pub name: String,
+//!      #[anorm(update)] // generate method update_xxx. 
+//!      pub password: String,
+//!      #[anorm(skip)] // ignore field
+//!       pub addr: Option<String>,
 //! }
 //! 
 //! 
